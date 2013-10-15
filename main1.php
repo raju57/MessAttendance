2a12dabb6894da0c10e99ee1f467dd9b1c041295
raju57 <rajusaratkar@gmail.com>
2013-10-15 16:36:59 +0530

    Create main1.php

---
+++ main1.php
@@ -0,0 +1,9 @@
+<html>
+    <body bgcolor="red">
+	     <a href="userlogin.php" style="text-decoration:none;"><input type="button" value="logout"></a>
+		 <a href="main1.php" style="text-decoration:none;"><input type="button" value="home"></a>	
+		 <br><br>
+		<iframe src="main2.php" width="630" height="460">
+		</iframe>
+    </body>
+</html>
