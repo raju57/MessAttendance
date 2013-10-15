cf795298bc4329f5c40f38d913cc4d3606a97b12
raju57 <rajusaratkar@gmail.com>
2013-10-15 16:36:05 +0530

    Create userlogin.php

---
+++ userlogin.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<br><br><br><br><br>
+<center>
+<form method="POST" action="mylogin.php">
+<table border="1">
+<tr>
+<td>
+username<input type="text" name="username" required>
+</td>
+</tr>
+<tr>
+<td>
+password<input type="password" name="password" required>
+</td>
+</tr>
+<tr>
+<td>
+<input type="submit" name="login">
+<input type="reset" name="reset"><br>
+</td>
+</tr>
+</table>
+</form>
+</center>
+</body>
+</html>
