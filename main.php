6c695e904dba072f59fc1ada606f9efa990cae2f
raju57 <rajusaratkar@gmail.com>
2013-10-15 16:33:30 +0530

    Create main.php

---
+++ main.php
@@ -0,0 +1,10 @@
+<html>
+<title>     MESS   </title>
+<div style="width:1330px;height:100px;background-color:#b0c4de;clear:both;margin-top:10px;padding-top:10px;padding-right:5px;padding-left:5px;">
+<head><h7><center> <b>NATIONAL INSTITUTE OF TECHNOLOGY TIRUCHIRAPPALLI</center> </h7></head><BR>
+<h7><center>STUDENT MESS ATTENDANCE MANAGEMENT SYSTEM</center></h7></b>
+</div>
+<body bgcolor="red">
+<iframe src="userlogin.php" width="1335" height="530"></iframe>
+</body>
+</html>
